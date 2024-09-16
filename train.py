4408772d4c0a4fd4fb0d6cb9f0c5c2244b571dbd
Farzanehkari <120786062+Farzanehkari@users.noreply.github.com>
2024-09-15 22:06:03 -0500

    train.py

--- train.py
+++ train.py
@@ -12,10 +12,8 @@ import random
 from tensorflow.keras.metrics import BinaryAccuracy
 from utils import *
 from utils import calculate_fps
-
 from models import *
-from tensorflow.keras.callbacks import LearningRateScheduler
-from tensorflow.keras.callbacks import ReduceLROnPlateau
+from tensorflow.keras.callbacks import LearningRateScheduler, ReduceLROnPlateau
 
 class PrintLR(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs=None):
@@ -33,9 +31,6 @@ def scheduler(epoch, lr):
 lr_scheduler = LearningRateScheduler(scheduler, verbose=1)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=1e-7, verbose=1)
 
-
-
-
 def initialize_loss_arrays(epochs):
     return [np.nan] * epochs
 
@@ -45,7 +40,6 @@ tf.random.set_seed(random_seed)
 random.seed(random_seed)
 
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
 
 input_size = (256, 256, 1)
 
@@ -57,8 +51,6 @@ def load_images_from_folder(folder):
             if img is not None:
                 images.append(np.array(img.convert('L')).reshape(256, 256, 1) / 255.0)  # Normalize to [0, 1]
     return images
-
-    
 
 results = {
     "Epoch": [],
@@ -76,16 +68,8 @@ results = {
     "SSIM": [],
     "F-measure": [],
     "DRD": [],
-    "FPS": []  
-
+    "FPS": []
 }
-
-
-def convert_grayscale_to_rgb(grayscale_image):
-    if grayscale_image.ndim == 2:
-        color_image = np.repeat(grayscale_image[:, :, np.newaxis], 3, axis=2)
-        return color_image
-    return grayscale_image
 
 def visualize_images(images, titles, rows=1, cols=3):
     fig, axes = plt.subplots(rows, cols, figsize=(15, 5))
@@ -96,11 +80,8 @@ def visualize_images(images, titles, rows=1, cols=3):
     plt.show()
 
 def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, callbacks=[]):
-
-
     train_deg_images = load_images_from_folder('Patch2013/A/train/')
     train_clean_images = load_images_from_folder('Patch2013/B/train-gt/')
-
     val_deg_images = load_images_from_folder('Patch2013/A/valid/')
     val_clean_images = load_images_from_folder('Patch2013/B/valid-gt/')
 
@@ -135,38 +116,34 @@ def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, ca
 
     for e in range(ep_start, epochs + 1):
         print(f'\n Epoch: {e}')
-
         total_d_loss_real, total_d_loss_fake = 0, 0
         total_generator_adversarial_loss, total_generator_pixelwise_loss, total_generator_perceptual_loss = 0, 0, 0
         val_total_d_loss_real, val_total_d_loss_fake = 0, 0
         val_total_generator_adversarial_loss, val_total_generator_pixelwise_loss, val_total_generator_perceptual_loss = 0, 0, 0
 
         for im in tqdm(range(len(train_deg_images)), desc=f"Epoch {e}", leave=False, disable=True):
-
             b_wat_batch = train_deg_images[im].reshape((1, 256, 256, 1))
             b_gt_batch = train_clean_images[im].reshape((1, 256, 256, 1))
             b_wat_batch = next(datagen.flow(b_wat_batch, batch_size=1, shuffle=False))
-        
+
             # Ensure normalized generated images
             generated_images = generator.predict(b_wat_batch)
-        
+
             valid = np.ones((b_gt_batch.shape[0], 16, 16, 1))
             fake = np.zeros((b_gt_batch.shape[0], 16, 16, 1))
-        
+
             discriminator.trainable = True
-            if im % 5 == 0:  
-                
+            if im % 5 == 0:
                 d_loss_real = discriminator.train_on_batch([b_gt_batch, b_wat_batch], valid)
                 d_loss_fake = discriminator.train_on_batch([generated_images, b_wat_batch], fake)
                 total_d_loss_real += d_loss_real[0]
                 total_d_loss_fake += d_loss_fake[0]
-        
+
             discriminator.trainable = False
             g_loss = gan.train_on_batch([b_wat_batch], [valid, b_gt_batch, b_gt_batch])
             total_generator_adversarial_loss += g_loss[1]
             total_generator_pixelwise_loss += g_loss[2]
             total_generator_perceptual_loss += g_loss[3]
-
 
         d_loss_real_arr.append(total_d_loss_real / (len(train_deg_images) / 2))
         d_loss_fake_arr.append(total_d_loss_fake / (len(train_deg_images) / 2))
@@ -180,9 +157,6 @@ def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, ca
         for im in range(len(val_deg_images)):
             b_wat_batch = val_deg_images[im].reshape((1, 256, 256, 1))
             b_gt_batch = val_clean_images[im].reshape((1, 256, 256, 1))
-
-            if len(b_wat_batch) != 1 or len(b_gt_batch) != 1:
-                continue
 
             generated_images = generator.predict(b_wat_batch)
             valid = np.ones((1, 16, 16, 1))
@@ -207,10 +181,9 @@ def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, ca
         print(f"Validation Losses - Discriminator Real: {val_d_loss_real_arr[-1]:.4f}, Fake: {val_d_loss_fake_arr[-1]:.4f}, "
               f"Adversarial: {val_generator_adversarial_loss_arr[-1]:.4f}, Pixel-wise: {val_generator_pixelwise_loss_arr[-1]:.4f}, Perceptual: {val_generator_perceptual_loss_arr[-1]:.4f}")
 
-        psnr_value, ssim_value, f_measure_value, drd_value, fps_value  = evaluate(generator, discriminator, e, val_deg_images, val_clean_images)
+        psnr_value, ssim_value, f_measure_value, drd_value, fps_value = evaluate(generator, val_deg_images, val_clean_images)
 
-
-
+        # Log the values into your `results` dictionary
         results["Epoch"].append(e)
         results["Train_Discriminator_Real_Loss"].append(d_loss_real_arr[-1])
         results["Train_Discriminator_Fake_Loss"].append(d_loss_fake_arr[-1])
@@ -226,15 +199,14 @@ def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, ca
         results["SSIM"].append(ssim_value)
         results["F-measure"].append(f_measure_value)
         results["DRD"].append(drd_value)
-        results["FPS"].append(fps_value)  
+        results["FPS"].append(fps_value)
 
+        generator.save_weights(f'weights/generator_epoch_{e}.weights.h5')
+        discriminator.save_weights(f'weights/discriminator_epoch_{e}.weights.h5')
 
-        generator.save_weights(f'weightsDIBCO2013/generator_epoch_{e}.weights.h5')
-        discriminator.save_weights(f'weightsDIBCO2013/discriminator_epoch_{e}.weights.h5')
-        
         visualize_images([b_wat_batch[0], generated_images[0], b_gt_batch[0]], 
                              ['Degraded', 'Generated', 'Ground Truth'])
-    
+
     df = pd.DataFrame(results)
     df.to_excel('training_results.xlsx', index=False)
 
@@ -242,7 +214,7 @@ def train_gan(generator, discriminator, ep_start=1, epochs=32, batch_size=64, ca
                 generator_perceptual_loss_arr, val_d_loss_real_arr, val_d_loss_fake_arr, val_generator_adversarial_loss_arr,
                 val_generator_pixelwise_loss_arr, val_generator_perceptual_loss_arr)
 
-def evaluate(generator, discriminator, epoch, val_deg_images, val_clean_images):
+def evaluate(generator, val_deg_images, val_clean_images):
     psnr_total = 0
     ssim_total = 0
     f_measure_total = 0
@@ -253,24 +225,18 @@ def evaluate(generator, discriminator, epoch, val_deg_images, val_clean_images):
         deg_image = val_deg_images[i].reshape(1, 256, 256, 1)
         clean_image = val_clean_images[i].reshape(1, 256, 256, 1)
         generated_image = generator.predict(deg_image)
-        
-        deg_image_rgb = convert_grayscale_to_rgb(deg_image.squeeze())
-        clean_image_rgb = convert_grayscale_to_rgb(clean_image.squeeze())
-        generated_image_rgb = convert_grayscale_to_rgb(generated_image.squeeze())
-        
-        psnr_total += psnr(clean_image_rgb, generated_image_rgb)
-        ssim_total += tf.reduce_mean(tf.image.ssim(tf.cast(clean_image_rgb, tf.float32), tf.cast(generated_image_rgb, tf.float32), max_val=1.0)).numpy()
-        f_measure_total += f_measure(clean_image_rgb, generated_image_rgb)
-        drd_total += drd(clean_image_rgb, generated_image_rgb)
-    
-    fps = calculate_fps(generator, val_deg_images)
-    
-    return psnr_total / num_images, ssim_total / num_images, f_measure_total / num_images, drd_total / num_images, fps
+
+        psnr_total += psnr(clean_image, generated_image)
+        ssim_total += ssim_metric(clean_image.squeeze(), generated_image.squeeze())
+        f_measure_total += f_measure(clean_image.squeeze(), generated_image.squeeze())
+        drd_total += drd(clean_image.squeeze(), generated_image.squeeze())
+        fps_total = calculate_fps(val_deg_images, val_clean_images)
+
+    return psnr_total / num_images, ssim_total / num_images, f_measure_total / num_images, drd_total / num_images, fps_total
 
 def plot_losses(epochs, d_loss_real_arr, d_loss_fake_arr, generator_adversarial_loss_arr, generator_pixelwise_loss_arr,
                 generator_perceptual_loss_arr, val_d_loss_real_arr, val_d_loss_fake_arr, val_generator_adversarial_loss_arr,
                 val_generator_pixelwise_loss_arr, val_generator_perceptual_loss_arr):
-
     epoch_range = range(1, len(d_loss_real_arr) + 1)
 
     plt.figure(figsize=(10, 10))
@@ -304,9 +270,17 @@ def plot_losses(epochs, d_loss_real_arr, d_loss_fake_arr, generator_adversarial_
     plt.legend()
     plt.title('Generator Perceptual Loss')
 
+    plt.subplot(3, 2, 6)
+    plt.plot(epoch_range, results['PSNR'], label='PSNR')
+    plt.plot(epoch_range, results['SSIM'], label='SSIM')
+    plt.plot(epoch_range, results['F-measure'], label='F-measure')
+    plt.plot(epoch_range, results['DRD'], label='DRD')
+    plt.plot(epoch_range, results['FPS'], label='FPS')
+    plt.legend()
+    plt.title('Metrics')
+
     plt.savefig('training_loss_plots.png')
     plt.close()
-
 
 def predict_and_visualize(generator, test_images_folder, output_folder):
     test_deg_images = load_images_from_folder(test_images_folder)
@@ -335,19 +309,16 @@ def predict_and_visualize(generator, test_images_folder, output_folder):
 
         imageio.imwrite(os.path.join(output_folder, f'predicted_{i}.bmp'), predicted_image)
 
-
 # Initialize models
 generator = generator_model(biggest_layer=1024)
 discriminator = discriminator_model()
 
 # Compile models
-discriminator.compile(optimizer=Adam(learning_rate=1e-5), loss='binary_crossentropy', metrics=[BinaryAccuracy()])
-generator.compile(optimizer=Adam(learning_rate=1e-5), loss=['mse', 'binary_crossentropy', perceptual_loss_fn], metrics=[psnr_metric, ssim_metric])
+discriminator.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5), loss='binary_crossentropy', metrics=[BinaryAccuracy()])
+generator.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5), loss=['mse', 'binary_crossentropy', perceptual_loss_fn], metrics=[psnr_metric, ssim_metric])
 
 # Train the GAN
-#train_gan(generator, discriminator, ep_start=2, epochs=3, batch_size=64)
-#train_gan(generator, discriminator, ep_start=2, epochs=3, batch_size=64, callbacks=[lr_scheduler])
 train_gan(generator, discriminator, ep_start=129, epochs=256, batch_size=128, callbacks=[lr_scheduler, reduce_lr])
 
 # Predict and visualize results
-predict_and_visualize(generator, 'Patch2013/A/test/', 'ResultsDIBCO2013/test_results/')
+predict_and_visualize(generator, 'PatchImages/A/test/', 'Results3/test_results/')
